4f1395fed8168017dac159eb7abeb8979f23e72e
marciomaia2020 <marciofernando.maia@yahoo.com.br>
2023-03-09 12:18:50 -0300

    Update some files

---
+++ script.js
@@ -0,0 +1,37 @@
+const pessoas = {
+    primeiroNome: "Marcio",
+    segundoNome: "Fernando",
+    ultimoNome: "Maia",
+    idade: 53,
+    cpf: 11752400879,
+    rg: 18833323,
+    escolaridade: ["2º grau completo", "Graduação em Analise de Sistemas", "Certificação Microsoft"],
+    ativo: "true"
+}
+//! VIZUALIZANDO AS INFORMAÇÕES DO OBJETO CRIADO (pessoas)
+//! Note que exitem VARIÁVEIS COM O MESMO NOME
+/*
+const primeiroNome = pessoas.primeiroNome;
+const segundoNome = pessoas.segundoNome;
+const ultimoNome = pessoas.ultimoNome;
+const idade = pessoas.idade;
+const cpf = pessoas.cpf;
+const rg = pessoas.rg;
+const escolaridade = pessoas.escolaridade[0];
+const ativo = pessoas.ativo;
+*/
+//!PARA A QUANTIDADE DE CÓDIGO ACIMA FAÇA ou seja um *******************CONSTRUCTOR*******************
+const saida = { primeiroNome, segundoNome, ultimoNome, idade, cpf, rg, escolaridade, ativo } = pessoas;
+console.log(saida);
+
+//* APENAS PARA SAIR NA CONSOLE
+/*
+console.log(primeiroNome); 
+console.log(segundoNome);
+console.log(ultimoNome);
+console.log(idade);
+console.log(cpf);
+console.log(rg);
+console.log(escolaridade);
+console.log(ativo);
+*/
